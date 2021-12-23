79a8b0030b6ea968579e588a568e17bada8dd756
Peka80 <petar.dragovic@gmail.com>
2021-12-23 18:07:25 +0100

    izmene url slika

--- cypress/integration/pomCreate.spec.js
+++ cypress/integration/pomCreate.spec.js
@@ -2,7 +2,7 @@
 
 import { authLogin } from '../page_objects/authLogin';
 import { header } from '../page_objects/header';
-import { createGal, createGallery } from '../page_objects/createGal';
+import { createGallery } from '../page_objects/createGal';
 
 const faker = require("faker");
 
@@ -10,11 +10,11 @@ describe('POM Create gallery', () => {
 
   let validEmail = 'petar.dragovic@gmail.com';
   let validPass = 'peksel2002';
-  let title = 'Cypress POM Create New Gallery';
+  let title = 'Technologies';
   let descript = 'Create new gallery with one picture.';
-  let imageUrl1 = 'https://i.pinimg.com/originals/45/50/14/455014371ee786da05d84d64e17ef28c.jpg';
-  let imageUrl2 = 'https://www.verstappenshop.com/include/thumb/thumb.php?src=/img/product/12_helm_2020__0010_levels_1_copy_1.jpg';
-  let imageUrl3 = 'https://i.ytimg.com/vi/NHKpK9iRiPk/maxresdefault.jpg';
+  let imageUrl1 = 'https://cdn.pixabay.com/photo/2015/01/08/18/24/children-593313_960_720.jpg';
+  let imageUrl2 = 'https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_960_720.jpg';
+  let imageUrl3 = 'https://cdn.pixabay.com/photo/2016/11/29/11/39/computer-1869236_960_720.jpg';
 
   let userData = {
     image: faker.image.city() + '.jpg',
@@ -34,14 +34,14 @@ describe('POM Create gallery', () => {
     cy.url().should('contains', '/create');
   });
 
-  xit('Create new gallery with one image', () => {
+  it.only('Create new gallery with one image', () => {
     createGallery.oneImageGallery(title, descript, imageUrl1);
 
-    cy.url().should('contains', '/');
+    cy.url().should('not.include', '/create');
     cy.get('h1').should('have.text', 'All Galleries');
   });
 
-  it.only('Create new gallery with 3 images', () => {
+  it('Create new gallery with 3 images', () => {
     createGallery.titleInput.type(title);
     createGallery.descriptInput.type(descript);
     createGallery.imgUrl.type(imageUrl1);
@@ -51,9 +51,9 @@ describe('POM Create gallery', () => {
     createGallery.imgUrl.eq(2).type(imageUrl3);
 
     createGallery.submitBtn.click();
-    cy.url().should('contains', '/');
-    cy.get('h1').should('have.text', 'All Galleries');
-  });
 
+    cy.url().should('not.include', '/create');
+    // .should('have.text', 'All Galleries');
+  });
 
 });
